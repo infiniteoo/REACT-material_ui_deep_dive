f8201f4b91f33cae94513ffc201e8b72c22f3900
Troy Dorman <troydorman@gmail.com>
2021-08-11 09:28:07 -0600

    implemented make/useStyles to add custom css styling to our components. used it on a button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,24 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import CheckBox from "@material-ui/core/CheckBox";
 import { useState } from "react";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B, #FF8E53)",
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: "white",
+    padding: "5px 30px",
+  },
+});
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>;
+}
 
 function CheckboxExample() {
   const [checked, setChecked] = useState(true);
@@ -35,6 +53,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonStyled />
+        <TextField
+          variant="filled"
+          color="secondary"
+          type="email"
+          label="The Time"
+          placeholder="test@test.com"
+        />
         <CheckboxExample />
         <ButtonGroup variant="contained" color="primary">
           <Button
