ea13575cf6b271edecdb698338553f43ec2ae4f3
Troy Dorman <troydorman@gmail.com>
2021-08-11 07:24:44 -0600

    created a button and added styling. installed material ui icons and added it to the button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          size="large"
+          startIcon={<SaveIcon />}
+          endIcon={<SaveIcon />}
+          variant="contained"
+          color="secondary"
+          href="#"
+          onClick={() => alert("hello")}
         >
-          Learn React
-        </a>
+          Hello World
+        </Button>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
   );
