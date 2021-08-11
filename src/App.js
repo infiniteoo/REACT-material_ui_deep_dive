5d307e24dfc00ee4e7f58be5c04b6b1b8df34f31
Troy Dorman <troydorman@gmail.com>
2021-08-11 07:46:48 -0600

    imported and implemented a basic checkbox

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
 import DeleteIcon from "@material-ui/icons/Delete";
+import CheckBox from "@material-ui/core/CheckBox";
+import { useState } from "react";
+
+function CheckboxExample() {
+  const [checked, setChecked] = useState(true);
+
+  return (
+    <div>
+      <CheckBox
+        checked={checked}
+        onChange={(e) => {
+          setChecked(e.target.checked);
+        }}
+        inputProps={{
+          "aria-label": "secondary checkbox",
+        }}
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckboxExample />
         <ButtonGroup variant="contained" color="primary">
           <Button
             size="large"
