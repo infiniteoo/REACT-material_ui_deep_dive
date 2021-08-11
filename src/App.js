ee41c2f8690b3cb841f351eae8ffcbe8c7d1297c
Troy Dorman <troydorman@gmail.com>
2021-08-11 07:28:17 -0600

    created buttongroup component and wrapped our buttons around them

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button
-          size="large"
-          startIcon={<SaveIcon />}
-          endIcon={<SaveIcon />}
-          variant="contained"
-          color="secondary"
-          href="#"
-          onClick={() => alert("hello")}
-        >
-          Hello World
-        </Button>
+        <ButtonGroup variant="contained" color="primary">
+          <Button
+            size="large"
+            startIcon={<SaveIcon />}
+            href="#"
+            onClick={() => alert("hello")}
+          >
+            Save
+          </Button>
+          <Button
+            size="large"
+            endIcon={<DeleteIcon />}
+            href="#"
+            onClick={() => alert("hello")}
+          >
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
